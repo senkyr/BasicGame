2e143c514cb69d2b8a449498801b72afbc824fc1
Jakub Šenkýř <senkyr@spstrutnov.cz>
2020-03-15 21:23:14 +0100

    Zobrazení statického polygonu

--- BasicEffectPolygons/Hra.cs
+++ BasicEffectPolygons/Hra.cs
@@ -7,7 +7,12 @@ namespace BasicEffectPolygons
     public class Hra : Game
     {
         GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        BasicEffect basicEffect;
+
+        VertexBuffer vertexBuffer;
+        Matrix world = Matrix.CreateTranslation(0, 0, 0);
+        Matrix view = Matrix.CreateLookAt(new Vector3(0, 0, 10), new Vector3(0, 0, 0), new Vector3(0, 1, 0));
+        Matrix projection = Matrix.CreateOrthographic(800f, 600f, 0.01f, 100f);
 
         public Hra()
         {
@@ -17,16 +22,26 @@ namespace BasicEffectPolygons
 
         protected override void Initialize()
         {
-            
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 600;
+            graphics.IsFullScreen = false;
+            graphics.ApplyChanges();
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            spriteBatch = new SpriteBatch(GraphicsDevice);
+            basicEffect = new BasicEffect(GraphicsDevice);
 
-            
+            vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(VertexPositionColor), 3, BufferUsage.WriteOnly);
+
+            VertexPositionColor[] vertices = new VertexPositionColor[3];
+            vertices[0] = new VertexPositionColor(new Vector3(0, 100, 0), Color.Black);
+            vertices[1] = new VertexPositionColor(new Vector3(-100f, -100f, 0), Color.DarkGray);
+            vertices[2] = new VertexPositionColor(new Vector3(+100f, -100f, 0), Color.Gray);
+
+            vertexBuffer.SetData<VertexPositionColor>(vertices);
         }
 
         protected override void Update(GameTime gameTime)
@@ -43,7 +58,22 @@ namespace BasicEffectPolygons
         {
             GraphicsDevice.Clear(Color.White);
 
-            
+            basicEffect.World = world;
+            basicEffect.View = view;
+            basicEffect.Projection = projection;
+            basicEffect.VertexColorEnabled = true;
+
+            GraphicsDevice.SetVertexBuffer(vertexBuffer);
+
+            RasterizerState rasterizerState = new RasterizerState();
+            rasterizerState.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = rasterizerState;
+
+            foreach(EffectPass pass in basicEffect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 1);
+            }
 
             base.Draw(gameTime);
         }
