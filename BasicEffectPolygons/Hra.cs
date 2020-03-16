ced6de1c568471d0f3188dd1efc44f50d44eb9d6
Jakub Šenkýř <senkyr@spstrutnov.cz>
2020-03-16 02:05:31 +0100

    Kód polygonu přesunutý do třídy

--- BasicEffectPolygons/Hra.cs
+++ BasicEffectPolygons/Hra.cs
@@ -6,44 +6,49 @@ namespace BasicEffectPolygons
 {
     public class Hra : Game
     {
-        GraphicsDeviceManager graphics;
-        BasicEffect basicEffect;
+        GraphicsDeviceManager SpravceZobrazovacihoZarizeni;
+        GraphicsDevice ZobrazovaciZarizeni => GraphicsDevice;
 
-        VertexBuffer vertexBuffer;
-        Matrix world = Matrix.CreateTranslation(0, 0, 0);
-        Matrix view = Matrix.CreateLookAt(new Vector3(0, 0, 10), new Vector3(0, 0, 0), new Vector3(0, 1, 0));
-        Matrix projection = Matrix.CreateOrthographic(800f, 600f, 0.01f, 100f);
+        Rectangle OknoHry = new Rectangle(0, 0, 800, 600);
+        Point StredOkna => OknoHry.Center;
 
-        float rotation = 0.0f;
+        Matrix Kamera;
+        Matrix Projekce;
+
+        Polygon Trojuhelnik;
 
         public Hra()
         {
-            graphics = new GraphicsDeviceManager(this);
+            SpravceZobrazovacihoZarizeni = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
 
         protected override void Initialize()
         {
-            graphics.PreferredBackBufferWidth = 800;
-            graphics.PreferredBackBufferHeight = 600;
-            graphics.IsFullScreen = false;
-            graphics.ApplyChanges();
+            SpravceZobrazovacihoZarizeni.PreferredBackBufferWidth = OknoHry.Width;
+            SpravceZobrazovacihoZarizeni.PreferredBackBufferHeight = OknoHry.Height;
+            SpravceZobrazovacihoZarizeni.IsFullScreen = false;
+            SpravceZobrazovacihoZarizeni.ApplyChanges();
+
+            RasterizerState nastaveniRasterizeru = new RasterizerState();
+            nastaveniRasterizeru.CullMode = CullMode.None;
+            ZobrazovaciZarizeni.RasterizerState = nastaveniRasterizeru;
+
+            Vector3 UmisteniKamery = new Vector3(StredOkna.X, StredOkna.Y, 1);
+            Vector3 NamireniKamery = new Vector3(StredOkna.X, StredOkna.Y, 0);
+            Vector3 NatoceniKamery = Vector3.UnitY;
+
+            Kamera = Matrix.CreateLookAt(UmisteniKamery, NamireniKamery, NatoceniKamery);
+            Projekce = Matrix.CreateOrthographic(OknoHry.Width, OknoHry.Height, 0.1f, 1.0f);
+
+            Trojuhelnik = new Polygon(3, 100, StredOkna.ToVector2(), 0, Color.Gray, ZobrazovaciZarizeni);
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            basicEffect = new BasicEffect(GraphicsDevice);
 
-            vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(VertexPositionColor), 3, BufferUsage.WriteOnly);
-
-            VertexPositionColor[] vertices = new VertexPositionColor[3];
-            vertices[0] = new VertexPositionColor(new Vector3(0, 100, 0), Color.Black);
-            vertices[1] = new VertexPositionColor(new Vector3(-100f, -100f, 0), Color.DarkGray);
-            vertices[2] = new VertexPositionColor(new Vector3(+100f, -100f, 0), Color.Gray);
-
-            vertexBuffer.SetData<VertexPositionColor>(vertices);
         }
 
         protected override void Update(GameTime gameTime)
@@ -51,31 +56,16 @@ namespace BasicEffectPolygons
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            rotation += 0.001f;
+            Trojuhelnik.Update(gameTime);
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.White);
+            ZobrazovaciZarizeni.Clear(Color.White);
 
-            basicEffect.World = Matrix.CreateRotationZ(rotation);
-            basicEffect.View = view;
-            basicEffect.Projection = projection;
-            basicEffect.VertexColorEnabled = true;
-
-            GraphicsDevice.SetVertexBuffer(vertexBuffer);
-
-            RasterizerState rasterizerState = new RasterizerState();
-            rasterizerState.CullMode = CullMode.None;
-            GraphicsDevice.RasterizerState = rasterizerState;
-
-            foreach(EffectPass pass in basicEffect.CurrentTechnique.Passes)
-            {
-                pass.Apply();
-                GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 1);
-            }
+            Trojuhelnik.Draw(Kamera, Projekce, ZobrazovaciZarizeni);
 
             base.Draw(gameTime);
         }
---
+++ BasicEffectPolygons/Polygon.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicEffectPolygons
+{
+    class Polygon
+    {
+        public int N { get; private set; }
+        public float Polomer { get; private set; }
+
+        private VertexPositionColor[] Vertexy { get; set; }
+        private BasicEffect Efekt { get; set; }
+
+        private Vector2 Pozice { get; set; }
+        private float Rotace { get; set; }
+        private Color Barva { get; set; }
+
+        public Polygon(int n, float polomer, Vector2 pozice, float rotace, Color barva, GraphicsDevice zobrazovaciZarizeni)
+        {
+            if (n < 3 || polomer <= 0)
+                throw new Exception("Neplatný n-úhelník.");
+
+            N = n;
+            Polomer = polomer;
+            Pozice = pozice;
+            Rotace = rotace;
+            Barva = barva;
+
+            Efekt = new BasicEffect(zobrazovaciZarizeni);
+            Vertexy = PripravitVertexy();
+        }
+
+        private VertexPositionColor[] PripravitVertexy()
+        {
+            float uhlovyPosun = (float)(2 * Math.PI / N);
+
+            Vector2[] vrcholy = new Vector2[N];
+
+            for (int i = 0; i < vrcholy.Length; i++)
+                vrcholy[i] = Vector2.Transform(new Vector2(0, Polomer), Matrix.CreateRotationZ(uhlovyPosun * i));
+
+            VertexPositionColor[] vertexy = new VertexPositionColor[N * 3];
+
+            for(int i = 0; i < vertexy.Length; i += 3)
+            {
+                int soucasny = i / 3;
+                int predchozi = (i / 3 - 1 + N) % N;
+
+                vertexy[i + 0] = new VertexPositionColor(new Vector3(0, 0, 0), Color.White);
+                vertexy[i + 1] = new VertexPositionColor(new Vector3(vrcholy[soucasny].X, vrcholy[soucasny].Y, 0), Barva);
+                vertexy[i + 2] = new VertexPositionColor(new Vector3(vrcholy[predchozi].X, vrcholy[predchozi].Y, 0), Barva);
+            }
+
+            return vertexy;
+        }
+
+        public void Update(GameTime casOdMinulehoUpdatu)
+        {
+            Rotace += 5 * (float)(Math.PI / 180 * casOdMinulehoUpdatu.ElapsedGameTime.TotalSeconds);
+        }
+
+        public void Draw(Matrix kamera, Matrix projekce, GraphicsDevice zobrazovaciZarizeni)
+        {
+            Efekt.World = Matrix.CreateRotationZ(Rotace) * Matrix.CreateTranslation(Pozice.X, Pozice.Y, 0);
+            Efekt.View = kamera;
+            Efekt.Projection = projekce;
+            Efekt.VertexColorEnabled = true;
+
+            foreach (EffectPass pruchod in Efekt.CurrentTechnique.Passes)
+            {
+                pruchod.Apply();
+
+                zobrazovaciZarizeni.DrawUserPrimitives(PrimitiveType.TriangleList, Vertexy, 0, N);
+            }
+        }
+    }
+}
